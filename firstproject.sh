12c91844b9704c41859f51268f13483d3c4ee53d
thejaswi2025 <thejaswiankireddypalli@gmail.com>
2025-03-20 07:42:34 +0000

    This is my first project

---
+++ firstproject.sh
@@ -0,0 +1,9 @@
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/thejaswi2025/devops.git
+git remote -v
+git push -u origin maingit commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/thejaswi2025/devops.git
+git remote -v
+git push -u origin main
